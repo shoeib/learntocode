23106a6e28fa03103c6088dcbe63dfe312683be7
shoeib <sh.mohamadirad@gmail.com>
2022-11-12 21:24:06 -0700

    chnaged list to dict

--- teaflask.py
+++ teaflask.py
@@ -3,17 +3,20 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-db = []
+db = dict()
 @app.route('/', methods=['POST'])
 def create_record():
-    record = json.loads(request.data)
-    print(record)
-    db.append(record)
+    db = json.loads(request.data)
+    print(db)
+    # db['id'] = record[]
+    # db['fname'] = record['fname']
+    # db['lname'] = record['lname']
     return jsonify('Record received!')
 
 
 @app.route('/', methods=['GET'])
 def query_records():
+    print(db)
     return jsonify(db)    
 
 @app.route('/data', methods=['GET'])
@@ -21,10 +24,10 @@ def query_srecords():
     fname = request.args.get('fname')
     # lname = request.args.get('lname')
     for item in db:
-        if item['name'] == fname:
+        if item['fname'] == fname:
             return jsonify({'Message':'name found!!!'})
     return jsonify({'Message': 'name not found!!!'})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
